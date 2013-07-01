80cb45cbb852abd02deae9abd7674742338884dc
Adèle Delamarche <adelamarche@mamasam.net>
2013-07-01 20:32:12 +0200

    Update settings.py

--- taskito/taskito/settings.default.py
+++ taskito/taskito/settings.default.py
@@ -1,4 +1,6 @@
 # Django settings for mysite project.
+import os
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -61,7 +63,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -124,6 +126,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'base',
+    'tasks',
 )
 
 # A sample logging configuration. The only tangible logging
